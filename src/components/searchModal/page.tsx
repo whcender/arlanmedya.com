b6bf4fdfdb383c61d073d98f1be5715de1c8fd25
whcender <heilender31@gmail.com>
2023-09-18 23:58:01 +0300

    search page

--- src/components/searchModal/page.tsx
+++ src/components/searchModal/page.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {AiOutlineClose} from 'react-icons/ai'
+import {AiOutlineSearch} from "react-icons/ai"
+import './style.css'
 
 interface SearchModalProps {
     setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -11,14 +14,16 @@ const Page: React.FC<SearchModalProps> = ({ setIsOpen }) => {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
+        animation: 'slideIn 0.5s ease',
     };
 
     return (
-        <div style={containerStyle} className='bg-black w-full h-full'>
-            <div className='flex items-center justify-center h-full'>
+        <div style={containerStyle} className='opacity-[97%] overflow-hidden bg-black w-full h-full'>
+            <div className='flex items-center justify-center gap-3 h-full'>
                 <input className='text-center outline-none border-none bg-black text-white text-2xl' type="text" placeholder='Yazmaya başlayın..' />
+                <button><AiOutlineSearch color="white" size="30"/></button>
             </div>
-            <button onClick={()=>{setIsOpen(false)}} className='absolute top-5 right-5 text-white'>X</button>
+            <button onClick={()=>{setIsOpen(false)}} className='absolute top-10 right-10 text-white'><AiOutlineClose size="30"/></button>
         </div>
     );
 };
